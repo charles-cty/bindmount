060db36c6ecc0f59bb2145932e0692032d1a5c9f
Charles Chen <67942242+charles-cty@users.noreply.github.com>
2026-08-22 17:26:33 +0800

    Preserve GUI launch working directory

--- scripts/bindmount-gui.ps1
+++ scripts/bindmount-gui.ps1
@@ -130,7 +130,7 @@ function Start-Bindmount([string[]]$Arguments) {
     $quotedArguments = $Arguments | ForEach-Object { Quote-ProcessArgument $_ }
     $argumentString = $quotedArguments -join ' '
     $process = Start-Process -FilePath $cli -ArgumentList $argumentString `
-        -WorkingDirectory (Split-Path -Parent $cli) -WindowStyle Normal -PassThru
+        -WorkingDirectory $currentWorkingDirectory -WindowStyle Normal -PassThru
     if ($process.WaitForExit(1500)) {
         if ($process.ExitCode -ne 0) {
             throw "The requested process could not be launched. The bindmount launcher reported exit code $($process.ExitCode)."
